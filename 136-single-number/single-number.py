c93d0167f4154e9749dd98e73f3c1eb2a774efe2
Gowri Shankar <146365390+Shankarkasarapu@users.noreply.github.com>
2025-08-23 21:06:31 +0530

    Time: 7 ms (42.53%) | Memory: 14.8 MB (12.59%) - LeetSync

---
+++ 136-single-number/single-number.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        hashmap= {}
+        for i in nums:
+            if i in hashmap:
+                hashmap[i]+=1
+            else:
+                hashmap[i]=1
+        for i in hashmap:
+            if hashmap[i]==1:
+                return i
+        
