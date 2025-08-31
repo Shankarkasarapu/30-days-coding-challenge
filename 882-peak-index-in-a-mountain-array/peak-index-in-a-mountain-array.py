29127fbcfdc0f88f54c7f72f91b6db19f771d44c
Gowri Shankar <146365390+Shankarkasarapu@users.noreply.github.com>
2025-08-31 22:19:48 +0530

    Time: 13 ms (12.60%) | Memory: 21.7 MB (60.01%) - LeetSync

---
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def peakIndexInMountainArray(self, arr):
+        # left =0 
+        # right = len(arr)-1
+        # while left<=right:
+        #     mid= (left+right)//2
+        #     if( )
+        x= max(arr)
+        return arr.index(x)
+        
