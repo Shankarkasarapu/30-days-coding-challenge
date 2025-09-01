083e1011df359ef9ea42afe9cc917f9f919405bd
Gowri Shankar <146365390+Shankarkasarapu@users.noreply.github.com>
2025-09-01 23:31:18 +0530

    Time: 37 ms (56.48%) | Memory: 13.9 MB (49.96%) - LeetSync

---
+++ 561-array-partition/array-partition.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def arrayPairSum(self, nums):
+        sum=0 
+        sorte=sorted(nums)
+        for i in range(0,len(sorte),2):
+            sum+=sorte[i]
+
+        return sum 
