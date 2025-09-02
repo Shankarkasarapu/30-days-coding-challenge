cec66d0ac8e543684a36fc003b2a628ec4547a30
Gowri Shankar <146365390+Shankarkasarapu@users.noreply.github.com>
2025-09-02 23:24:57 +0530

    Time: 218 ms (61.88%) | Memory: 15.2 MB (42.06%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def shipWithinDays(self, weights, days):
+        mincap = max(weights)
+        maxcap = sum(weights)
+
+        while mincap<maxcap :
+            mid=(mincap+maxcap)/2
+            D,curr_sum=1,0
+            for w in weights :
+                if w+curr_sum>mid:
+                    D+=1
+                    curr_sum=0
+                curr_sum+=w
+            if D>days :
+                mincap=mid+1
+            else:
+                maxcap=mid
+        return mincap
+            
